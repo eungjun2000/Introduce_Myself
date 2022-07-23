9f93eec1caeff218cc11a2570cbe57471eeb0fb9
eungjun2000 <eungjun2000@gmail.com>
2022-07-23 21:14:25 +0900

    introduce_myself

--- Main_JS.js
+++ Main_JS.js
@@ -19,26 +19,57 @@ menu_btn.addEventListener('click', () => {
     blur.classList.toggle('blur_main');
 })
 
-/* Lottie animation */
-var project_file_image_anim = bodymovin.loadAnimation({
-    container: document.querySelector('.project_file'),
+/*------------------------------- Lottie animation -------------------------------*/
+
+/* Birth part */
+
+/* Code */
+
+/* Project part */
+var project_animation = bodymovin.loadAnimation({
+    container: document.querySelector('.project_span'),
     renderer: 'svg',
     loop: false,
     autoplay: false,
-    path: 'Project_file_image.json'
+    path: 'Project_anim.json'
 });
 
 let project_block = document.getElementById("block_3");
-let project_file_updown = document.getElementById("project");
+let project_updown = document.getElementById("project");
 
-project_block.addEventListener('mouseover', () => {
-    project_block.onmouseover = function(){
-        project_file_updown.style.transform = "translateY(-30px)";
+project_block.onmouseover = function(){
+    project_updown.style.transform = "translateY(-30px)";
+    project_animation.playSegments([0,69], true);
+}
+project_block.onmouseout = function(){
+    project_updown.style.transform = "translateY(0)";
+}
+
+/* Academic background part */
+var arcademic_background_animation = bodymovin.loadAnimation({
+    container: document.querySelector('.academic_background_span'),
+    renderer: 'svg',
+    loop: false,
+    autoplay: false,
+    path: 'Academic_background_anim.json'
+});
+
+let academic_background_block = document.getElementById("block_8");
+let academic_background_updown = document.getElementById("academic_background");
+var count = 0;
+
+academic_background_block.onmouseover = function(){
+    academic_background_updown.style.transform = "translateY(-30px)";
+    arcademic_background_animation.playSegments([0,69], true);
+    
+    arcademic_background_animation.onComplete = function(){
+        arcademic_background_animation.playSegments([30,68], true);
     }
-    project_file_image_anim.playSegments([0,69], true);
-})
-project_block.addEventListener('mouseout', () => {
-    project_block.onmouseout = function(){
-        project_file_updown.style.transform = "translateY(0)";
+}
+academic_background_block.onmouseout = function(){
+    academic_background_updown.style.transform = "translateY(0)";
+    arcademic_background_animation.playSegments([10,0], true);
+    arcademic_background_animation.onComplete = function(){
+        arcademic_background_animation.stop();
     }
-})
+}
